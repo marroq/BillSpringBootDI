b2438d33b4fbf9744f8214c02ae92e50825f62d7
Douglas Figueroa <dffigueroa@galileo.edu>
2024-08-07 21:25:04 -0600

    Initial commit

---
+++ src/main/java/com/dfigueroa/di/BillSpringBootDI/BillSpringBootDiApplication.java
@@ -0,0 +1,13 @@
+package com.dfigueroa.di.BillSpringBootDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSpringBootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSpringBootDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfigueroa/di/BillSpringBootDI/BillSpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfigueroa.di.BillSpringBootDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
