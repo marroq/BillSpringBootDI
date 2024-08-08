71ee2175e26d6e00023257febf1b21a98af1d4e0
Douglas Figueroa <dffigueroa@galileo.edu>
2024-08-07 21:53:21 -0600

    feat: creating model classes, Client, Invoice, Item and Product

---
+++ src/main/java/com/dfigueroa/di/BillSpringBootDI/model/Item.java
@@ -0,0 +1,23 @@
+package com.dfigueroa.di.BillSpringBootDI.model;
+
+public class Item {
+
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
